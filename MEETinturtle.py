6dde9bedbcc268e369779159e8317b2192a59cc6
ahmadf21-meet <ahmadf21@meet.mit.edu>
2019-07-21 09:34:49 -0400

    Stopping here

---
+++ MEETinturtle.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.penup()
+turtle.goto(-200,-100)
+turtle.pendown()
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,40)
+turtle.goto(-200+100,60+40)
+turtle.goto(-200+100,-100)
+turtle.penup()
+turtle.goto(-50,-100)
+turtle.pendown()
+turtle.goto(-50,-100+200)
+turtle.goto(-50,-100)
+turtle.goto(-50+100,-100)
+turtle.goto(-50,-100)
+turtle.goto(-50,-100+100)
+turtle.goto(-50+100,0)
+
+turtle.mainloop()
